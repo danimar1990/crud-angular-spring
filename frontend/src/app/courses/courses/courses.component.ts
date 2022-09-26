e6205d47b28f281a0321f242f47a077f2fb91386
Danimar Varisa <danimarvarisa@gmail.com>
2022-09-25 23:06:53 -0300

    Criação da tela de novo curso e ajuste de rotas

--- frontend/src/app/courses/courses/courses.component.ts
+++ frontend/src/app/courses/courses/courses.component.ts
@@ -5,6 +5,7 @@ import { catchError, Observable, of } from 'rxjs';
 
 import { Course } from '../model/course';
 import { CoursesService } from '../services/courses.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-courses',
@@ -13,21 +14,29 @@ import { CoursesService } from '../services/courses.service';
 })
 export class CoursesComponent implements OnInit {
   courses$: Observable<Course[]>;
-  displayedColumns = ['_id', 'name', 'category'];
+  displayedColumns = ['_id', 'name', 'category', 'actions'];
 
-  constructor(private coursesService: CoursesService, public dialog: MatDialog) {
-    this.courses$ = this.coursesService.getAllCourses()
-    .pipe(
-      catchError(error => {
+  constructor(
+    private coursesService: CoursesService,
+    public dialog: MatDialog,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
+    this.courses$ = this.coursesService.getAllCourses().pipe(
+      catchError((error) => {
         this.onError('Ocorreu um erro ao carregar os cursos!');
-        return of([])
+        return of([]);
       })
     );
   }
 
+  onAdd() {
+    this.router.navigate(['new'], { relativeTo: this.route });
+  }
+
   onError(errorMessage: string) {
     this.dialog.open(ErrorDialogComponent, {
-      data: errorMessage
+      data: errorMessage,
     });
   }
 
