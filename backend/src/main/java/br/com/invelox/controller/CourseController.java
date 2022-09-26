8c21edff48ddeddc43742648310df29a326d4443
Danimar Varisa <danimarvarisa@gmail.com>
2022-09-26 00:43:28 -0300

    Finalização do cadastro do curso, tanto no backend como frontend

--- backend/src/main/java/br/com/invelox/controller/CourseController.java
+++ backend/src/main/java/br/com/invelox/controller/CourseController.java
@@ -3,10 +3,9 @@ package br.com.invelox.controller;
 import br.com.invelox.model.Course;
 import br.com.invelox.repository.CourseRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,7 +17,13 @@ public class CourseController {
     private CourseRepository courseRepository;
 
     @GetMapping
-    public List<Course> courses() {
+    public List<Course> list() {
         return courseRepository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course) {
+        return courseRepository.save(course);
     }
 }
