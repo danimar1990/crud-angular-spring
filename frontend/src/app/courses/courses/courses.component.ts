613aa097c79c35e7a2aa8b555736aa448cbaee75
Danimar Varisa <danimarvarisa@gmail.com>
2022-09-26 01:42:33 -0300

    Refatoração da tabela para mat-table e estilização CSS

--- frontend/src/app/courses/courses/courses.component.ts
+++ frontend/src/app/courses/courses/courses.component.ts
@@ -14,7 +14,6 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class CoursesComponent implements OnInit {
   courses$: Observable<Course[]>;
-  displayedColumns = ['_id', 'name', 'category', 'actions'];
 
   constructor(
     private coursesService: CoursesService,
