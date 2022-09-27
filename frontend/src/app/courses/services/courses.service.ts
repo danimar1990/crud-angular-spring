f4282b44f55d2fa2f4b0cf61956e4492a40273a1
Danimar Varisa <danimarvarisa@gmail.com>
2022-09-26 22:46:51 -0300

    Possibilitando salvamento do formulário tanto para criação quanto para edição

--- frontend/src/app/courses/services/courses.service.ts
+++ frontend/src/app/courses/services/courses.service.ts
@@ -13,11 +13,7 @@ export class CoursesService {
   constructor(private httpClient: HttpClient) {}
 
   getAllCourses() {
-    return this.httpClient.get<Course[]>(this.API).pipe(
-      first(),
-      delay(1000),
-      tap((courses) => console.log(courses))
-    );
+    return this.httpClient.get<Course[]>(this.API).pipe(first(), delay(1000));
   }
 
   loadById(id: string) {
@@ -25,6 +21,19 @@ export class CoursesService {
   }
 
   save(course: Partial<Course>) {
+    if (course._id) {
+      return this.update(course);
+    }
+    return this.create(course);
+  }
+
+  private create(course: Partial<Course>) {
     return this.httpClient.post<Course>(this.API, course).pipe(first());
+  }
+
+  private update(course: Partial<Course>) {
+    return this.httpClient
+      .put<Course>(`${this.API}/${course._id}`, course)
+      .pipe(first());
   }
 }
